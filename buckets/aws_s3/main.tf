92876dd51fb96b048b858d92e2dacd7b00bf40cf
RAsheek <sallanrasheek@gmail.com>
2024-07-02 12:19:16 +0530

    aws s3 bucket with versioning and ss encryption

--- buckets/aws_s3/main.tf
+++ buckets/aws_s3/main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket_versioning" "enabled" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption" "default" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.terraform_state.id
 
   rule {
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket                  = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
   block_public_policy     = true
-  ignore_bucket_acls      = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -40,7 +40,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
-    name = "LOCKID"
+    name = "LockID"
     type = "S"
   }
 }
