d391bfad911c07e325fc10ba955f0bce0ec536a5
RAsheek <sallanrasheek@gmail.com>
2024-07-03 12:00:16 +0530

    reconfigured backend in main.tf

--- buckets/aws_s3/backend.tf
+++
@@ -1,4 +0,0 @@
-bucket         = "terraform-upand-running-state"
-region         = "us-east-2"
-dynamodb_table = "terraform-up-and-running-locks"
-encrypt        = true
--- buckets/aws_s3/main.tf
+++ buckets/aws_s3/main.tf
@@ -47,6 +47,9 @@ resource "aws_dynamodb_table" "terraform_locks" {
 terraform {
   backend "s3" {
     key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-terraform_locks"
+    encrypt        = true
   }
 }
 
