ef6c2fc5367232ec716cb0b985fa32f59e5afd32
RAsheek <sallanrasheek@gmail.com>
2024-07-03 12:51:41 +0530

    modified lock filesS

--- isolation/via_workspace/main.tf
+++ isolation/via_workspace/main.tf
@@ -12,7 +12,7 @@ terraform {
     bucket         = "terraform-up-and-running-state-rasheek"
     key            = "workspaces-example/terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "terraform-up-and-running_locks"
+    dynamodb_table = "terraform-up-and-running-terraform_locks"
     encrypt        = true
   }
 }
