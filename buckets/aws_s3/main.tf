7a286aedb1f3d09634054c9bd136fc6806e74304
RAsheek <sallanrasheek@gmail.com>
2024-07-03 12:05:00 +0530

    corrected bucket name

--- buckets/aws_s3/main.tf
+++ buckets/aws_s3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state-"
+  bucket = "terraform-up-and-running-state-rasheek"
   lifecycle {
     prevent_destroy = true
   }
