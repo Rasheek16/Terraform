416795592fc29e21ed1addf48a2b3770206807c3
RAsheek <sallanrasheek@gmail.com>
2024-07-03 12:35:43 +0530

    added terraform backend

--- isolation/via_workspace/main.tf
+++ isolation/via_workspace/main.tf
@@ -6,3 +6,13 @@ resource "aws_instance" "example_workspace" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state-rasheek"
+    key            = "workspaces-example/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running_locks"
+    encrypt        = true
+  }
+}
