751015f32cf0de4e4acb2501d66b1a56857d5b44
RAsheek <sallanrasheek@gmail.com>
2024-07-04 10:14:08 +0530

    added terraform backend

--- isolation/file_system/stage/services/data-stores/mysql/main.tf
+++ isolation/file_system/stage/services/data-stores/mysql/main.tf
@@ -13,3 +13,13 @@ resource "aws_db_instance" "example" {
   username = var.db_username
   password = var.db_password
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state-rasheek"
+    key            = "stage/datas-stores/mysql/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-terraform_locks"
+    encrypt        = true
+  }
+}
