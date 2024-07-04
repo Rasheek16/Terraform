64ac4eef577733977f720dfc118b09c841037219
RAsheek <sallanrasheek@gmail.com>
2024-07-05 01:00:36 +0530

    added more outputs

--- module/modules/services/webserver-cluster/outputs.tf
+++ module/modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,7 @@ output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "Domain name of the load balancer"
 }
+output "asg_name" {
+  value = aws_autoscaling_group.example.name
+  description = "Name of the ASG"
+}
